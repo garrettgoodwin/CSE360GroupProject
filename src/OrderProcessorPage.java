7c7f9513b7ac62bf694aac44f2ba64819567b5a3
Garrett Goodiwn <50435035+garrettgoodwin@users.noreply.github.com>
2022-11-12 14:17:57 -0700

    Create OrderProcessorPage.java

---
+++ src/OrderProcessorPage.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class OrderProcessorPage extends SceneController
+{
+    @FXML
+    public ListView orderListView;  //List to link view
+
+    //Looking into th elistview bc I think its what we want for this
+}
