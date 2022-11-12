40275b584a67883335f0374cad4f84997e7410e7
Garrett Goodiwn <50435035+garrettgoodwin@users.noreply.github.com>
2022-11-12 14:19:21 -0700

    Create OrderStatusPage.java

---
+++ src/OrderStatusPage.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class OrderStatusPage extends SceneController
+{
+    @FXML
+    public TextField emailField;
+}
