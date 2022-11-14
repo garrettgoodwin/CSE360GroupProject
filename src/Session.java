2db042a8712f49b4a9360aab3c209ab42956c68f
James Weber <72582898+jamesweber7@users.noreply.github.com>
2022-11-13 21:49:24 -0700

    placeorder with asurite

--- src/Session.java
+++ src/Session.java
@@ -19,10 +19,47 @@ public class Session {
         login(login);
         return login.getResponse();
     }
-    public Response createAccount(String username, String password, String name, String email, int phoneNumber, int asurite) {
+    public Response createAccount(String username, String password, String name, String email, String phoneNumber, String asurite) {
         // check for non-database-related valid entered information (e.g. username length, etc)
         // Database.createAccount will conduct database-related exception checking
         //      (e.g. username already exists in database)
+        Response response = validate(username, password, name, email, phoneNumber, asurite);
+        if (!Response.ok(response)) {
+            return response;
+        }
+        // if passes surface tests
+        Login login = Database.createAccount(username, password, name, email, phoneNumber, Integer.parseInt(asurite));
+        if (login.isAccepted()) {
+            login(login);
+        }
+        return login.getResponse();
+    }
+
+    public void continueAsGuest() {
+        Login login = Database.createGuestSession();
+        login(login);
+    }
+
+    /* (Admin) : Create an account of any type */
+    /* types: Admin, Chef, OrderProcessor, Customer */
+    public Response createAccount(String username, String password, String name, String email, String phoneNumber, String asurite, int type) {
+        // check for non-database-related valid entered information (e.g. username length, etc)
+        // Database.createAccount will conduct database-related exception checking
+        //      (e.g. username already exists in database)
+        Response response = validate(username, password, name, email, phoneNumber, asurite);
+        if (!Response.ok(response)) {
+            return response;
+        }
+
+        return Database.createAccount(this.getId(), username, password, name, email, phoneNumber, Integer.parseInt(asurite), type);
+    }
+
+    /* (Admin) : Give permissions (change type of) an account via User email */
+    public Response givePermissions(String email, int type) {
+        return Database.givePermissions(this.getId(), email, type);
+    }
+
+    private Response validate(String username, String password, String name, String email, String phoneNumber, String asurite) {
         Response response;
         response = User.Username.validate(username);
         if (!Response.ok(response)) {
@@ -36,14 +73,21 @@ public class Session {
         if (!Response.ok(response)) {
             return response;
         }
-
-        // if passes surface tests
-        Login login = Database.createAccount(username, password, name, email, phoneNumber, asurite);
-        if (login.isAccepted()) {
-            login(login);
+        response = User.PhoneNumber.validate(phoneNumber);
+        if (!Response.ok(response)) {
+            return response;
         }
-        return login.getResponse();
+        response = User.Name.validate(name);
+        if (!Response.ok(response)) {
+            return response;
+        }
+        response = User.Asurite.validate(asurite);
+        if (!Response.ok(response)) {
+            return response;
+        }
+        return Response.OK;
     }
+
     public int getId() {
         return id;
     }
@@ -53,8 +97,33 @@ public class Session {
     public Order getOrder() {
         return order;
     }
+    public Response placeOrder(String cardNumber, String cardholderName, String exp, int cvv, int deliveryMethod) {
+        Response response = Payment.validate(cardNumber, exp, Integer.toString(cvv));
+        if (Response.ok(response)) {
+            order.setStatus(Order.ACCEPTED);
+            order.setDeliveryMethod(deliveryMethod);
+            Database.saveOrder(order, this.getId());
+            savePaymentMethod(cardNumber, exp, cardholderName, cvv);
+        }
+        return response;
+    }
+    public Response placeOrder(int asurite, int deliveryMethod) {
+        Response response = User.Asurite.validate(Integer.toString(asurite));
+        if (Response.ok(response)) {
+            order.setStatus(Order.ACCEPTED);
+            order.setDeliveryMethod(deliveryMethod);
+            Database.saveOrder(order, this.getId());
+        }
+        return response;
+    }
+    public float calculateTotal() {
+        return order.calculateTotal();
+    }
     public int getOrderId() {
         return order.getId();
+    }
+    public boolean orderIsBlank() {
+        return getOrder() == Order.BLANK;
     }
     public boolean getIsClosed() {
         return isClosed;
@@ -63,6 +132,7 @@ public class Session {
         if (login.isAccepted()) {
             setId(login.sessionId);
             setUser(login.userId);
+            setOrder(login.orderId);
         }
     }
     private void setId(int id) {
@@ -74,6 +144,16 @@ public class Session {
     }
     private void setUser(User user) {
         this.user = user;
+    }
+    private void setOrder(int orderId) {
+        Order order = Database.getOrder(orderId);
+        setOrder(order);
+    }
+    private void setOrder(Order order) {
+        this.order = order;
+    }
+    public String getOrderStatusText() {
+        return order.getStatusText();
     }
     /* user role */
     public boolean isGuest() {
@@ -98,9 +178,17 @@ public class Session {
         return user.isEmployee();
     }
     /* pizza customization */
-    public void addPizza(int pizzaType, boolean mushrooms, boolean olives, boolean onions, boolean extraCheese, int quantity) {
+    /* return pizza id */
+    public int addPizza(int pizzaType, boolean mushrooms, boolean olives, boolean onions, boolean extraCheese, int quantity) {
         Pizza pizza = Pizza.create(getId(), getOrderId(), pizzaType, mushrooms, olives, onions, extraCheese, quantity);
         order.addPizza(pizza);
+        return pizza.getId();
+    }
+    public void updatePizza(int pizzaId, int pizzaType, boolean mushrooms, boolean olives, boolean onions, boolean extraCheese, int quantity) {
+        order.updatePizza(pizzaId, pizzaType, mushrooms, olives, onions, extraCheese, quantity);
+    }
+    public void removePizza(int pizzaId) {
+        order.removePizza(pizzaId);
     }
     /* order retreival */
     public Order[] getSavedOrders() {
@@ -113,5 +201,60 @@ public class Session {
     // returns empty [] if not chef or admin
     public Order[] getOrdersForCooking() {
         return Database.getOrdersForCooking(this.getId());
+    }
+
+    /* Order Processor */
+    public void markOrderReadyToCook(int orderId) {
+        Database.markOrderReadyToCook(orderId, this.getId());
+    }
+    /* Chef */
+    public void markOrderCooking(int orderId) {
+        Database.markOrderCooking(orderId, this.getId());
+    }
+    /* Chef */
+    public void markOrderReady(int orderId) {
+        Database.markOrderReady(orderId, this.getId());
+        sendEmailToCustomer(orderId);
+    }
+    /* I don't think this will ever be used in the actual application */
+    public void markOrderPickedUp(int orderId) {
+        Database.markOrderPickedUp(orderId, this.getId());
+    }
+    
+    /* saved payment methods */
+    public void savePaymentMethod(String cardNumber, String exp, String cardholderName, int cvv) {
+        Database.createPayment(user.getId(), cardNumber, exp, cardholderName, cvv);
+    }
+    public Payment[] getSavedPaymentMethods() {
+        return Database.getSavedPaymentMethods(user.getId(), this.getId());
+    }
+
+    public void sendEmailToCustomer(int orderId) {
+        Order order = Database.getOrder(orderId);
+        String email = Database.getOrderCustomerEmail(orderId, this.getId());
+        String name = Database.getOrderCustomerName(orderId, this.getId());
+        String status = order.getStatusText();
+        String subject = "SunDevil Pizza: Your Order is " + status;
+        String body = name + ",\n"
+            + "your SunDevil Pizza order is " + status + ".\n"
+            + order.getReceipt();
+        User.Email.sendEmail(email, subject, body);
+    }
+
+    /* Checking Order Status while not logged in */
+    public void setOrderForStatus(int orderId) {
+        setOrder(Database.getOrder(orderId));
+    }
+
+    public void setOrderForStatus(String username, String password) {
+        setOrder(Database.getLastPlacedOrder(username, password));
+    }
+    
+    @Override
+    public String toString() {
+        String str = "Session #" + getId() + "\n\n";
+        str += getUser().toString() + "\n\n";
+        str += getOrder().toString();
+        return str;
     }
 }
